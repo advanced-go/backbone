36cad4ec4e1eab5c2c335afa90f123b56090a731
advanced-go <mark.wilson.go@outlook.com>
2024-06-10 13:54:29 -0500

    Initial commit

---
+++ agent/agent.go
@@ -0,0 +1,35 @@
+package agent
+
+import (
+	"github.com/advanced-go/stdlib/messaging"
+)
+
+// All agents utilize: observations, guidance, and experience as input to inference -> to create actions
+// observations, guidance, experience -> inference -> actions
+
+// NewCaseOfficer - assigned to a region/zone/sub-zone, managing unique hosts. Case officer agent manages
+// host agents
+func NewCaseOfficer(uri string, assignment any, ctrlHandler messaging.Handler) (messaging.Agent, error) {
+	return nil, nil
+}
+
+// NewHostAgent - agent responsible for a specific host in region/zone/sub-zone/host. Host agent manages
+// ingress, egress traffic, and egress routing agents
+func NewHostAgent(uri string, assignment any, ctrlHandler messaging.Handler) (messaging.Agent, error) {
+	return nil, nil
+}
+
+// NewIngressAgent - ingress traffic controller for a host, one agent per host.
+func NewIngressAgent(uri string, assignment any, ctrlHandler messaging.Handler) (messaging.Agent, error) {
+	return nil, nil
+}
+
+// NewEgressTrafficAgent - egress traffic controller for a host, one agent per route
+func NewEgressTrafficAgent(uri string, assignment any, ctrlHandler messaging.Handler) (messaging.Agent, error) {
+	return nil, nil
+}
+
+// NewEgressRoutingAgent - egress traffic routing for a host, one agent per rout
+func NewEgressRoutingAgent(uri string, assignment any, ctrlHandler messaging.Handler) (messaging.Agent, error) {
+	return nil, nil
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module gihub.com/advanced-go/backbone
 
 go 1.21.4
+
+require github.com/advanced-go/stdlib v0.0.0-20240610172656-d35294ea5a10 // indirect
